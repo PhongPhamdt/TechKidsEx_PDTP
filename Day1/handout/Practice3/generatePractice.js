e9062625572bc80a89a21193255dcd53f4232de7
Marvelous <phongpham.hedspi@gmail.com>
2018-08-13 19:23:20 +0700

    Fixed

--- Day1/handout/Practice3/generatePractice.js
+++ Day1/handout/Practice3/generatePractice.js
@@ -9,34 +9,34 @@
 //     })
 //   ); // Remove this line and change to your own algorithm
 // }
-// function getRandomInt(a, b) {
-//   return Math.floor(Math.random() * (b - a)) + a;
-// }
+function getRandomInt(a, b) {
+  return Math.floor(Math.random() * (b - a)) + a;
+}
 
-// function generate(testLengthArray) {
-//   var list = new Array;
-//   var count = 0;
-//   while (count < testLengthArray.length) {
-//     var input = new Array;
-//     while (input.length < testLengthArray[count]) {
-//       input.push(getRandomInt(-10000, 10000));
-//     }
-//     input = bubbleSort(input);
-//     var target = generateTarget(input);
-//     var output = search(input, target);
-//     list.push({ "input": input, "target": target, "output": output });
-//     console.log("input: [" + input +"]\n"+ "target: " + target +"\n"+ "output: " + output+"\n");
-//     count++;
-//   }
-//   // console.log(testLengthArray.length);
-//   return list;
-// }
+function generate(testLengthArray) {
+  var list = new Array;
+  var count = 0;
+  while (count < testLengthArray.length) {
+    var input = new Array;
+    while (input.length < testLengthArray[count]) {
+      input.push(getRandomInt(-10000, 10000));
+    }
+    input = bubbleSort(input);
+    var target = generateTarget(input);
+    var output = search(input, target);
+    list.push({ "input": input, "target": target, "output": output });
+    console.log("input: [" + input +"]\n"+ "target: " + target +"\n"+ "output: " + output+"\n");
+    count++;
+  }
+  // console.log(testLengthArray.length);
+  return list;
+}
 
-// function generateTarget(input) {
-//   if(input.length==1)
-//   	return input[0];
-//   else return input[getRandomInt(0,input.length-1)];
-// }
+function generateTarget(input) {
+  if(input.length==1)
+  	return input[0];
+  else return input[getRandomInt(0,input.length-1)];
+}
 
 function search(input, target) {
   for (var i = 0; i < input.length; i++) {
@@ -64,14 +64,6 @@ function sort(input) {
   // return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
   return bubbleSort(input);
 }
-function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
-    })
-  ); // Remove this line and change to your own algorithm
-}
+
 
 module.exports = generate
