f159481fa52d7dda2ec58af826cdb0b3bd608cf0
Marvelous <phongpham.hedspi@gmail.com>
2018-09-22 12:59:50 +0700

    Update Minihack

--- Day5/App/Client/js/index.js
+++ Day5/App/Client/js/index.js
@@ -1,68 +1,70 @@
-// $(document).ready(function() {
-// 	$.ajax({
-// 		url: 'http://localhost:6969/question',
-// 		method: 'GET',
-// 		success: function(data) {
-// 			console.log("Success!", data.question);
-// 			// $('h1').replaceWith(data);
-// 			$('h1').html(data.question);
-// 			$('.answer').attr("data-id",data.question.id);
-// 		},
-// 		error: function() {
-// 			console.log("FAIL !!!");
-// 		}
-// 	});
-
-// 	$('.answer').on('click', function(e) {
-// 		let answer = $(e.target).attr('data-answer');
-// 		let questionId = $(e.target).attr('data-id');
-// 		console.log(answer, questionId);
-// 	});
-// })
 $(document).ready(function() {
   $.ajax({
     url: 'http://localhost:6969/question',
     method: 'GET',
     success: function(data) {
-      $('h1').text(data.question.questionContent);
-      $('.answer').attr("data-id", data.question.id);
+      $('#questionContent').text(data.question.content);
+      $('.answer').attr("data-id", data.question._id);
     },
     error: function() {
       console.log("fail!");
     }
   });
 
-  $('.answer').on('click', function(e) {
-    let answer = $(e.target).attr('data-answer');
-    let questionId = $(e.target).attr('data-id');
-    console.log(answer, questionId);
-    $.ajax({
-  		url: 'http://localhost:6969/answer',
-  		method: 'PUT',
-  		data : {
-                answer,
-                questionId,
-            },
-  		success: function(data) {
-  			console.log("Success!");
-  		},
-  		error: function() {
-  			console.log("fail!");
-  		}
-  	});
-  });
-
-  // $.ajax({
-  //       url: 'http://localhost:6969/answer',
-  //       method: 'POST',
-  //       dataType: 'JSON',
-  //       success: function (data) {
-  //           $('h1').text(data.question.questionContent);
-  //           $('h3').attr('data-id', data.question.id)
-  //       },
-  //       error: function () {
-  //           console.log("error!");
-  //       }
-  //   });
+	$('.answer').on('click', function(e) {
+	let answer = $(e.target).attr('data-answer');
+	let questionId = $(e.target).attr('data-id');
+	console.log(answer, questionId);
+	$.ajax({
+			url: 'http://localhost:6969/answer',
+			method: 'PUT',
+			data : {
+	            answer,
+	            questionId,
+	        },
+			success: function(data) {
+				// console.log("Success!");
+				if(data.question){
+					let totalVote = data.question.yes + data.question.no; 
+					$('#vote').text(totalVote);
+					$('#voteYes').text(((data.question.yes/totalVote)*100).toFixed(2));
+					$('#voteNo').text(((data.question.no/totalVote)*100).toFixed(2));
+					$('.questionInfo').css('display', 'block');
+					$('.answers').css('display', 'none');
+				}
+			},
+			error: function() {
+				console.log("fail!");
+			}
+		});
+	});
+	$('#viewQuestionInfo').on('click', function (e) {
+		let questionId = $('.answer:first-child').attr('data-id');
+		$.ajax({
+			url: `http://localhost:6969/question/${questionId}`,
+			// url: 'http://localhost:6969/question/'+,     cách 2
+			method: 'GET',
+			success: function (data) {
+				if (data.question) {
+					let totalVote = data.question.yes + data.question.no;
+					$('#vote').text(totalVote);
+					$('#voteYes').text(((data.question.yes / totalVote) * 100).toFixed(2));
+					$('#voteNo').text(((data.question.no / totalVote) * 100).toFixed(2));
+					if(totalVote==0){
+					// console.log("cc ="+ ((data.question.yes / totalVote) * 100).toFixed(2));
+						$('.question_result_yes').css('width', '50%');
+						$('.question_result_no').css('width', '50%');
+					}else{
+						$('.question_result_yes').css('width', ((data.question.yes / totalVote) * 100).toFixed(2)+"%");
+						$('.question_result_no').css('width', ((data.question.no / totalVote) * 100).toFixed(2)+"%");
+					}
+					$('.questionInfo').css('display', 'block');
+					$('.answers').css('display', 'none');
+				}
+			},
+			error: function (error) {
+			console.log(error);
+			}
+		})
+	});
 });
-
--- Day5/App/Server/server.js
+++ Day5/App/Server/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const QuestionModel = require('./models/questionModel');
+const QuestionRouter = require('./router/questionRouter');
+
+mongoose.connect('mongodb://localhost/App', (err) => {
+	if(err) console.log(err);
+	else console.log("DB connect Success !!!");
+});
 
 let app = express();
 
@@ -9,83 +18,37 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+	res.send("Hello world");
 });
 
 app.post('/ask', (req, res) => {
-  fs.readFile('./questions.txt', (err, fileData) => {
-    if(err) console.log(err)
-    else {
-      try {
-        let questions = [];
-        if(fileData.length > 0 && JSON.parse(fileData).length) {
-          questions = JSON.parse(fileData);
-        }
-        const newQuestion = {
-          id: questions.length + 1,
-          questionContent: req.body.question,
-          yes: 0,
-          no: 0
-        }
-        questions.push(newQuestion);
-        fs.writeFile('./questions.txt', JSON.stringify(questions), (err) => {
-          if(err) console.log(err)
-          else res.redirect('http://localhost:8080/');
-        });
-      } catch (error) {
-        console.log("Error: ", error);
-      }
-    }
-  });
+	const newQuestion = {
+		content: req.body.question
+	}
+	QuestionModel.create(newQuestion, (err, questionCreated) => {
+		if(err) console.log(err);
+		else res.redirect('http://localhost:8080/');
+	});
 });
 
-app.get('/question', (req, res) => {
-  fs.readFile('./questions.txt', (err, fileData) => {
-    if(err) console.log(err);
-    else {
-      try {
-        let questions = JSON.parse(fileData);
-        let randomNum = Math.floor(Math.random()*questions.length);
-        let randomQuestion = questions[randomNum];
-        res.send({ message: "Success!", question: randomQuestion });
-      } catch (error) {
-        console.log("Error!!! ", error);
-      }
-    }
-  });
-});
+app.use('/question', QuestionRouter);
 
 app.put('/answer', (req, res) => {
-  console.log(req.body);
-  const { answer, questionId } = req.body;
-  fs.readFile('./questions.txt', (err, fileData) => {
-  	if(err) console.log(err);
-  	else{
-  		try{
-  			let questions = [];
-            if (fileData.length && JSON.parse(fileData).length) {
-            	questions = JSON.parse(fileData);
-            }
-                
-            // if(question.answer=='yes'){
-            //     questions[questionId-1].yes++;
-            // }else{
-            //     questions[questionId-1].no++;
-            // }
-            questions[questionId-1][answer]++;
-                
-            fs.writeFile('./questions.txt', JSON.stringify(questions), (err) => {
-                if (err) console.log(err)
-                else res.send({ message: "Success", questionId });
-            });
-  		} catch (error) {
-  			console.log("Error!!! ", error);
-  		}
-  	}
-  });
+	const {answer, questionId} = req.body;
+	QuestionModel.findById(questionId, (err, questionFound) => {
+		if(err) console.log(err);
+		if(!questionFound) res.send({ message: 'Not Found', question: null});
+		else{
+			questionFound[answer]++;
+			questionFound.save((err, questionFound) => {
+				if(err) console.log(err);
+				else res.send({message:"Success", question: questionFound});
+			});
+		}
+	});
 });
 
 app.listen(6969, (err) => {
-  if(err) console.log(err)
-  else console.log("Server is listening at port 6969!");
+	if(err) console.log(err);
+	else console.log("Server is listening at port 6969!");
 });
